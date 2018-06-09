f91ba06106628dd8a725246b28015fd920e21045
Denis Fäcke <serayaeryn@gmail.com>
2018-06-09 10:21:15 +0200

    add bin to gitignore

--- bin/src/BeanDefinition.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-Object.defineProperty(exports, "__esModule", { value: true });
-class BeanDefinition {
-    constructor(id) {
-        this.id = id;
-    }
-}
-exports.default = BeanDefinition;
