f4186aa7a6dd8f38cb9bcc375d37af76e8712d26
Denis Fäcke <serayaeryn@gmail.com>
2021-06-03 18:07:28 +0200

    fix ci

--- testdata/scan3.ts
+++ testdata/scan3.ts
@@ -1,6 +1,6 @@
 import { Component } from '../src/revane/Revane'
 
-@Component({dependencies: ['scan1'], id: 'scan3'})
+@Component({ dependencies: ['scan1'], id: 'scan3' })
 export class Scan3 {
   arg: any
   constructor (arg) {
